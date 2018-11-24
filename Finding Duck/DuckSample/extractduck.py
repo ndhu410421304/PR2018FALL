22d1b3c77560bf901ca24fcd61c87c5d95a88cc9
ndhu410421304 <410421304@gms.ndhu.edu.tw>
2018-11-25 01:25:05 +0800

    Pre model

---
+++ Finding Duck/DuckSample/extractduck.py
@@ -0,0 +1,26 @@
+import sys
+import cv2 as cv
+import numpy as np
+
+class Gmodel:
+	def __init__(self):
+		self.u = 0
+		self.co = 0
+		#self.p = 0
+	def likelihood(self, r, g, b):
+		p = 0
+		return p
+	def pox(self, likelihood):
+		pass
+
+class Bayes:
+	def __init__(self):
+		self.w0 = 1
+		self.w1 = 1
+	def classify(self, p)
+		label = 0
+		return label
+		
+
+
+
