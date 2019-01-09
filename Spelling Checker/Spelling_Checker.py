2b76d19b0eeebaf62f6b052c7c19e6bb1b0ce893
ndhu410421304 <410421304@gms.ndhu.edu.tw>
2019-01-10 02:51:14 +0800

    Rename and testing readfile

---
+++ Spelling Checker/Spelling_Checker.py
@@ -0,0 +1,8 @@
+#Spelling Checker
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon, QDesktopServices
+
+f = open("dictionary.txt", "r") 
+#print(f.read()) 
+print(f.readline()) 
