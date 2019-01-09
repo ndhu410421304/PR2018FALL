aa0e7d1415c3a3f9652654bf52d95c2d8a637b89
ndhu410421304 <410421304@gms.ndhu.edu.tw>
2019-01-10 02:55:48 +0800

    Counts lines

--- Spelling Checker/Spelling_Checker.py
+++ Spelling Checker/Spelling_Checker.py
@@ -5,4 +5,8 @@ from PyQt5.QtGui import QIcon, QDesktopServices
 
 f = open("dictionary.txt", "r") 
 #print(f.read()) 
-print(f.readline()) 
+#print(f.readline())
+linenum = 0;
+for line in f:
+	linenum+=1
+print(linenum)
