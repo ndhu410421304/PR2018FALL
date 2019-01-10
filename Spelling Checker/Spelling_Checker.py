c567657e5d919013cec7876e647bc2be1fd37a23
ndhu410421304 <410421304@gms.ndhu.edu.tw>
2019-01-11 03:05:54 +0800

    modify version

--- Spelling Checker/Spelling_Checker.py
+++ Spelling Checker/Spelling_Checker.py
@@ -8,10 +8,15 @@ f = open("dictionary.txt", "r")
 #print(f.read()) 
 #print(f.readline())
 D = numpy.zeros((40, 40))
+D1 = []
+D2 = []
+D3 = []
+
 word = input('Enter your Word:')
 #print(word)
 minDistance = 10000 #big enough when initial
 closestword = word
+equal = 0
 #print(len(word))
 I = len(word)
 linenum = 0;
@@ -42,11 +47,30 @@ for line in f:
 			D[i,j] = min(c1, c2, c3)
 
 	Distance = D[I-1,J-1]
-	if(Distance < minDistance):
-		minDistance = Distance
-		closestWord = B
+	#if(Distance < minDistance):
+		#minDistance = Distance
+		#closestWord = B
+	
 		#print(B)
 		#print(minDistance)
-
-print('Closest Word:',closestWord)	
+	if(Distance == 3):
+		D3.append(B)
+	if(Distance == 2):
+		D2.append(B)
+	if(Distance == 1):
+		D1.append(B)
+	if(Distance == 0):
+		closestword = B
+		equal = 1
+		
+if(equal == 1):
+	print('The spelling is correct!')
+else:
+	print('Spelling is wrong.')
+	print('Word with distance 1:')
+	print(D1)
+	print('Word with distance 2:')
+	print(D2)
+	print('Word with distance 3:')
+	print(D3)
 #print(linenum)
